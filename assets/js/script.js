09987f94614452376392f91eca848869ed72989f
Andressa Martins <martins.andressa13@gmail.com>
2022-04-06 16:27:08 -0300

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,34 @@
-alert("Olá");
+// trocar cor da div a partir dos clicks nos botões. 
 
+function verde() {
+    document.querySelector('#exemplo').classList.remove('azul')
+    document.querySelector('#exemplo').classList.remove('vermelho')
+    document.querySelector('#exemplo').classList.add('verde')
+    document.querySelector('#exemplo').innerHTML = 'Verde'
+}
+function vermelho() {
+    document.querySelector('#exemplo').classList.remove('verde')
+    document.querySelector('#exemplo').classList.remove('azul')
+    document.querySelector('#exemplo').classList.add('vermelho')
+    document.querySelector('#exemplo').innerHTML = 'Vermelho'
+}
+function azul() {
+    document.querySelector('#exemplo').classList.remove('verde')
+    document.querySelector('#exemplo').classList.remove('vermelho')
+    document.querySelector('#exemplo').classList.add('azul')
+    document.querySelector('#exemplo').innerHTML = 'Azul'
+}
+
+// Trocar cor do botão //
+
+function trocar() {
+
+   if (document.querySelector('#botao').classList.contains('branco')) {
+       document.querySelector('#botao').classList.remove('branco');
+       document.querySelector('#botao').classList.add('vermelho');
+   } else {
+    document.querySelector('#botao').classList.remove('vermelho');
+     document.querySelector('#botao').classList.add('branco');
+
+   }
+}
